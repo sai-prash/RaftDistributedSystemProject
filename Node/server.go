050d3a7f724e6e1ad937ee36cc3058d495989f53
sai-prash <s_saiprashanth@ymail.com>
2022-03-29 02:24:05 -0400

    Added Hotel Reservation source code

--- Node/server.go
+++ Node/server.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"net"
+	"net/http"
 	"os"
 	"time"
 )
@@ -25,6 +28,20 @@ type jsonMessage struct {
 	Term        int    `json: "term"`
 	Key         string `json: "key"`
 	Value       string `json: "value"`
+}
+
+type Rooms struct {
+	Rooms []RoomType `json: "Rooms"`
+}
+
+type RoomType struct {
+	Type  string   `json: "type"`
+	Dates []string `json: "Dates"`
+}
+
+type Booking struct {
+	RoomType string   `json: "RoomType"`
+	Dates    []string `json: "Dates"`
 }
 
 // type voteReq struct {
@@ -72,7 +89,7 @@ func main() {
 	if nodeName == "Node1" {
 		//time.Sleep(2000 * time.Millisecond)
 		//raftInfo.Timeout += 1000
-		currentServerType = Leader
+		//currentServerType = Leader
 	}
 	//Add random timeout
 	raftInfo.Timeout += rand.Intn(100)
@@ -90,7 +107,15 @@ func main() {
 		return
 	}
 	defer ser.Close()
-	handleRequest(ser, p)
+	go handleRequest(ser, p)
+
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/index.html", serveFile2)
+	http.HandleFunc("/getReservation", getReservation)
+	http.HandleFunc("/makeReservation", checkandUpdateReservation)
+	http.HandleFunc("/updateNodes", updateNodes)
+	fmt.Println("Server started at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 	fmt.Println("\n\n\n---Server Shutdown---\n\n\n")
 
@@ -342,4 +367,114 @@ func sendVoteResponse(reqData jsonMessage, conn *net.UDPConn, addr *net.UDPAddr)
 	if err != nil {
 		fmt.Printf("Couldn't send response %v", err)
 	}
+}
+
+//Hotel Reservation source code
+
+func getReservation(w http.ResponseWriter, r *http.Request) {
+	log.Println("dsdsdsdssdds")
+	//connection()
+	jsonFile, _ := os.Open("./data.json")
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &result)
+	w.Header().Set("Content-Type", "application/json")
+	jsonResp, _ := json.Marshal(result)
+	w.Write(jsonResp)
+
+}
+
+func checkandUpdateReservation(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("INSIDE CHECK N UPDATE")
+	NodeList := [3]string{"http://node3:8080", "http://node2:8080", "http://node1:8080"}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(NodeList[i] + "/updateNodes")
+		req, err := http.NewRequest("POST", NodeList[i]+"/updateNodes", bytes.NewBuffer([]byte(body)))
+		req.Header.Set("Content-Type", "application/json")
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("response Status:", resp.Status)
+		fmt.Println("response Headers:", resp.Header)
+	}
+
+}
+
+func updateNodes(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("------------Updating NODES----------")
+
+	jsonFile, _ := os.Open("data.json")
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var rooms Rooms
+	json.Unmarshal(byteValue, &rooms)
+
+	rmType := rooms.Rooms
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(body))
+	var b Booking
+	err = json.Unmarshal([]byte(body), &b)
+	fmt.Println("---", b)
+
+	for i := 0; i < len(rmType); i++ {
+		fmt.Println("King", rmType[i].Type)
+		if rmType[i].Type != b.RoomType {
+			continue
+		}
+		dates := rmType[i].Dates
+		fmt.Println("")
+		for j := 0; j < len(dates); j++ {
+			if contains(b.Dates, dates[j]) {
+				dates = append(dates[:j], dates[j+1:]...)
+				break
+			}
+		}
+		rmType[i].Dates = dates
+	}
+	rooms.Rooms = rmType
+	w.Header().Set("Content-Type", "application/json")
+	jsonResp, _ := json.Marshal(rooms)
+	fmt.Println(rooms)
+	w.Write(jsonResp)
+	os.WriteFile("data.json", []byte(string(jsonResp)), 0644)
+
+}
+
+func serveFile(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./index.html")
+}
+
+func serveFile2(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./index.html")
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+	http.ServeFile(w, r, "./index.html")
+}
+
+//Add it to seprate util file
+func contains(s []string, t string) bool {
+	for _, a := range s {
+		if a == t {
+			return true
+		}
+	}
+	return false
 }
