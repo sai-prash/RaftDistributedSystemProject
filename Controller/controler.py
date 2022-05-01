96a198533fcdd626e7ded0b07e44467616eac3e5
Deepak0797 <hariharishdeepak@gmail.com>
2022-05-01 18:38:52 -0400

    changes made for controller request

--- Controller/controler.py
+++ Controller/controler.py
@@ -90,10 +90,6 @@ except:
 
 
 time.sleep(5)
-
-#Initialize Request to get the leader info:
-sender = "Controller"
-port = 5555
 target="Node1"
 msg['sender_name'] = sender
 msg['request'] = "LEADER_INFO"
@@ -104,18 +100,10 @@ skt = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 skt.bind((sender, port))
 
 
-
 # Send Message
 try:
     # Encoding and sending the message
     skt.sendto(json.dumps(msg).encode('utf-8'), (target, port))
-    responsedata,server=skt.recvfrom(1024)
-    print(responsedata)
-    response=responsedata.decode("UTF-8")
-    response_dict=ast.literal_eval(response)
-    print(response_dict)
-    leader=response_dict["Value"]
-    print(f"leader node is :{leader}")
 except:
     #  socket.gaierror: [Errno -3] would be thrown if target IP container does not exist or exits, write your listener
     print(f"ERROR WHILE SENDING REQUEST ACROSS : {traceback.format_exc()}")
